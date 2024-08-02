17f9c3e07223fed56e9b1c2f109ef4adab3906af
dev-celo <marcelohenrique.backend@gmail.com>
2024-08-02 17:00:16 -0300

    Feat: Criando PUT na userRepository.

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -15,10 +15,40 @@ public class UserRepository
         return query;
     }
 
+    public User GetUser(int id)
+    {
+        try {
+            return _context.Users.Find(id);
+        }
+        catch (Exception ex) {
+            BadRequestResult(ex.Message);
+            return null;
+        }
+    }
+
     public User AddUser(User user)
     {
         _context.Add(user);
         _context.SaveChanges();
         return user;
+    }
+
+    public User UpdateUser(int id, User updatedUser)
+    {
+        var user = _context.Users.Find(id);
+
+        if (user != null)
+        {
+            user.UserName = updatedUser.UserName;
+            user.Email = updatedUser.Email;
+
+            _context.SaveChanges();
+        }
+        return user;
+    }
+
+    private void BadRequestResult(string message)
+    {
+        throw new NotImplementedException();
     }
 }
