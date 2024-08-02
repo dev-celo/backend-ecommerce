7a4ff3674e7b8caa8b40aa1819101043d61669d8
dev-celo <marcelohenrique.backend@gmail.com>
2024-08-01 21:50:37 -0300

    Feat: Rota get list user e post new user inciada.

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,7 @@ namespace backend_ecommerce.Models
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public ICollection<Order> Orders { get; set; }
+        public string PasswordHash { get; set; }
+        public ICollection<Order>? Orders { get; set; }
     }
 }
