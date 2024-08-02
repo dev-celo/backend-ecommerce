9cbb29801918414c53ff312e1af0954f2c9505ca
dev-celo <marcelohenrique.backend@gmail.com>
2024-08-02 16:59:01 -0300

    Feat: Criando PUT na userController.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -20,10 +20,30 @@ public class UserController : ControllerBase
         return Ok(res);
     }
 
+    [HttpGet("{id}")]
+    public IActionResult GetUser(int id)
+    {
+        var res = _repository.GetUser(id);
+        if (res == null) {
+            return NotFound();
+        }
+        return Ok(res);
+    }
+
     [HttpPost]
     public IActionResult AddUser (User user)
     {
         _repository.AddUser(user);
         return Ok(new { message = "User added"});
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(int id, User updatedUser)
+    {
+        var res = _repository.UpdateUser(id, updatedUser);
+        if (res == null) {
+            return NotFound();
+        }
+        return Ok(res);
     }
 }
