c200cad33e3a89487f8f11c3ad70592aa31ca862
dev-celo <marcelohenrique.backend@gmail.com>
2024-08-13 10:58:38 -0300

    Update: Melhorando a legibilidade do UserDTO

--- Services/AuthDto.cs
+++ Services/AuthDto.cs
@@ -1,6 +1,6 @@
 namespace Auth.Dto;
 
-public class LoginDTORequest
+public class UserDTO
 {
     public string? UserName { get; set; }
     public string Password { get; set; }
@@ -10,4 +10,21 @@ public class LoginDTORequest
 public class LoginDTOResponse
 {
     public string? Token { get; set; }
+}
+
+public class ChangePasswordDTO
+{
+    public string CurrentPassword { get; set; }
+    public string NewPassword { get; set; }
+}
+
+public class ForgotPasswordDTO
+{
+    public string Email { get; set; }
+}
+
+public class ResetPasswordDTO
+{
+    public string Token { get; set; }
+    public string NewPassword { get; set; }
 }
