47edf3cb9b4abf62e4f35ebbd5f9cf4a13e602b3
dev-celo <marcelohenrique.backend@gmail.com>
2024-08-13 11:00:23 -0300

    update: Melhorando rotas relacionadas a segurança.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,6 +5,7 @@ using Auth.Dto;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 
 [ApiController]
 [Route("[controller]")]
@@ -31,7 +32,7 @@ public class UserController : ControllerBase
 
     [HttpGet("{id}")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    [Authorize(Policy = "Admin, User")]
+    [Authorize(Policy = "Admin")]
     public IActionResult GetUserById(int id)
     {
         var res = _repository.GetUserById(id);
@@ -68,12 +69,13 @@ public class UserController : ControllerBase
 
     [HttpPost("{login}")]
     [AllowAnonymous]
-    public IActionResult Login(LoginDTORequest loginDTO)
+    public IActionResult Login(UserDTO loginDTO)
     {
         User? existingUser = _repository.GetUserByEmail(loginDTO.Email);
 
         if (existingUser == null) return Unauthorized(new { message = "Credenciais de Login incorretas" });
-        if (existingUser.PasswordHash != loginDTO.Password) return Unauthorized(new { message = "Credenciais de Login incorretas" });
+        var hash = CustomPasswordHasher.HashPassword(loginDTO.Password, out _);
+        if (hash != existingUser.PasswordHash) return Unauthorized(new { message = "Credenciais de Login incorretas" });
 
         var token = _tokenGenerator.Generate(existingUser);
         return Ok(new { token });
@@ -83,8 +85,80 @@ public class UserController : ControllerBase
     [AllowAnonymous]
     public IActionResult AddUser([FromBody] User user)
     {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        //Verificar se o usuário já existe
+        var existingUser = _repository.GetUserByEmail(user.Email);
+
+        if(existingUser != null)
+        {
+            return BadRequest(new { message = "Invalid email"});
+        }
+        
+       // Hash a senha e obter o salt
+        string salt;
+        user.PasswordHash = CustomPasswordHasher.HashPassword(user.PasswordHash, out salt);
+        user.Salt = salt;
+        user.Access = "User";
+
+        
         _repository.AddUser(user);
+
         var token = _tokenGenerator.Generate(user);
         return Created("", new { token });
+    }
+
+    // Usado quando o usuário deseja alterar a senha enquanto ainda está logado no sistema.
+    [HttpPost("change-password")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "User")]
+    public IActionResult ChangePassword(ChangePasswordDTO changePasswordDTO)
+    {
+        var UserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+        if (!_repository.ChangePassword(UserId, changePasswordDTO.CurrentPassword, changePasswordDTO.NewPassword))
+        {
+            return BadRequest(new { message = "Senha atual incorreta" });
+        }
+
+        return Ok(new { message = "Senha alterada com sucesso" });
+    }
+
+    // Usado quando o usuário esqueceu a senha.
+    [HttpPost("forgot-password")]
+    public IActionResult ForgotPassword([FromBody] ForgotPasswordDTO forgotPasswordDTO)
+    {
+        var user = _repository.GetUserByEmail(forgotPasswordDTO.Email);
+        if (user == null)
+        {
+            return NotFound(new { message = "User not found" });
+        }
+
+        var token = _tokenGenerator.Generate(user);
+        // Lógica para enviar email com o token
+        
+        return Ok(new { message = "Password reset link has been sent to your email." });
+    }
+    // Geralmente faz parte do processo de forgot password.
+    [HttpPost("reset-password")]
+    public IActionResult ResetPassword([FromBody] ResetPasswordDTO resetPasswordDTO)
+    {
+        var claimsPrincipal = _tokenGenerator.ValidateToken(resetPasswordDTO.Token);
+        if (claimsPrincipal == null)
+        {
+            return Unauthorized(new { message = "Invalid or expired token" });
+        }
+
+        var userId = int.Parse(claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier).Value);
+        var user = _repository.GetUserById(userId);
+        if (user == null)
+        {
+            return NotFound(new { message = "User not found" });
+        }
+
+        _repository.UpdatePassword(user.Email, resetPasswordDTO.NewPassword);
+        return Ok(new { message = "Password has been reset successfully." });
     }
 }
