bc606082b3fb4aa62af6aa73d42cfbfced1cd426
dev-celo <marcelohenrique.backend@gmail.com>
2024-10-28 18:49:42 -0300

    Feat: Adicionando um dbSet Category.

--- data/EcommerceContext.cs
+++ data/EcommerceContext.cs
@@ -16,6 +16,7 @@ public class EcommerceContext : DbContext
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
