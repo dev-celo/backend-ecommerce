8435bb1562f7f413073e5670e017cff99cf1a74f
dev-celo <marcelohenrique.backend@gmail.com>
2024-12-10 19:13:28 -0300

    Feat: Implementando seedData e migrations

--- DatabaseInitialize.cs
+++ DatabaseInitialize.cs
@@ -1,8 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 public static class DatabaseInitializer
 {
     public static void Initialize(EcommerceContext context)
     {
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
+        context.Database.Migrate();
     }
 }
