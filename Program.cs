64a5c1e04634e534479e12719c485b6540bbb878
dev-celo <marcelohenrique.backend@gmail.com>
2024-12-10 17:57:17 -0300

    Fix: Corrigindo problema ciclico com jsonSerializer

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using System.Security.Claims;
 using System.Text;
 using Auth.Constants;
+using backend_ecommerce.repository;
 using backend_ecommerce.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +15,21 @@ builder.Services.AddDbContext<EcommerceContext>();
 builder.Services.AddScoped<EcommerceContext>();
 builder.Services.AddScoped<UserRepository>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Configura o Serializador JSON para usar o ReferenceHandler.Preserve
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true; // opcional, para uma saída mais legível
+    });
+
+builder.Services.AddScoped<OrderRepository>();  // Remover duplicação
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configuraçoes de conjunto de políticas JWT Authorization
+// Configurações de conjunto de políticas JWT Authorization
 builder.Services.AddAuthorization(options => 
 {
     options.AddPolicy("Admin", policy => policy.RequireClaim(ClaimTypes.Role, "Admin"));
@@ -37,20 +49,21 @@ builder.Services.AddAuthentication(option =>
     {
         ValidateIssuer = true,
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidateAudience = false,
+        ValidateAudience = false,  // Configurar em seguida
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true, 
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"])) // Chave secreta
     };
 });
-builder.Services.AddScoped<TokenGenerator>();
+
+builder.Services.AddScoped<TokenGenerator>();  // Verifique se está sendo usado
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var DbContext = scope.ServiceProvider.GetRequiredService<EcommerceContext>();
-    DatabaseInitializer.Initialize(DbContext);
+    var dbContext = scope.ServiceProvider.GetRequiredService<EcommerceContext>();  // Alterado para camelCase
+    DatabaseInitializer.Initialize(dbContext);
 }
 
 // Configure the HTTP request pipeline.
